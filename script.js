40ec177a7359e5ace6ba39f9a98a1bb0da481d86
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2018-08-10 12:21:15 +0200

    fix nav&border

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-$('.carousel').carousel()
+$('.carousel').carousel();
+
+function fixNav() {
+        if(window.pageYOffset >= 557) {
+            $('#navbar').removeClass('navbar-border');
+        } else {
+        	$('#navbar').addClass('navbar-border');
+        };
+        if(window.pageYOffset >= 20) {
+            $('#navbar').addClass('navbar-md');
+        } else {
+        	$('#navbar').removeClass('navbar-md');
+        };
+};
+
+$(window).on('scroll', fixNav);
